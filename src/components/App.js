7d0e1a2b6c27a3ff88b78ad7294980c7a8864ca7
Glenn Reyes <glenn@glennreyes.com>
2019-04-02 16:40:17 +0200

    Add dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,10 @@ import { ThemeProvider } from 'styled-components';
 import Container from './Container';
 import ThemeSwitch from './ThemeSwitch';
 import Dimensions from './Dimensions';
+import { useDarkMode } from '../hooks';
 
 const App = () => {
-  const { darkMode, toggleDarkMode } = {
-    darkMode: false,
-    toggleDarkMode: () => {}
-  };
+  const { darkMode, toggleDarkMode } = useDarkMode(false);
   const size = { width: 0, height: 0 };
   const hidden = false;
 

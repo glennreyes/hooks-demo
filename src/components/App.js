f0de07327bf366b90188f947e4c63965fd89b90f
Glenn Reyes <glenn@glennreyes.com>
2019-04-02 16:48:13 +0200

    Add useHidden

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import Container from './Container';
 import ThemeSwitch from './ThemeSwitch';
 import Dimensions from './Dimensions';
-import { useDarkMode } from '../hooks';
+import { useDarkMode, useWindowSize } from '../hooks';
+
+const useHidden = (timeout = 1000, dependencies) => {
+  const [hidden, setHidden] = useState(true);
+
+  useEffect(() => {
+    setHidden(false);
+    const t = setTimeout(() => setHidden(true), timeout);
+
+    return () => clearTimeout(t);
+  }, dependencies);
+
+  return hidden;
+};
 
 const App = () => {
   const { darkMode, toggleDarkMode } = useDarkMode(false);
-  const size = { width: 0, height: 0 };
-  const hidden = false;
+  const size = useWindowSize();
+  const hidden = useHidden(2000, [size.width, size.height]);
 
   return (
     <ThemeProvider theme={{ darkMode }}>

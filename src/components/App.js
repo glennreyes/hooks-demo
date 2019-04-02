90c150d7e941c4511d3651b070d7bc6bc4d58a7f
Glenn Reyes <glenn@glennreyes.com>
2019-04-02 16:16:35 +0200

    Implement window size

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import Container from './Container';
 import ThemeSwitch from './ThemeSwitch';
 import Dimensions from './Dimensions';
 
 const App = () => {
-  const size = { width: 0, height: 0 };
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  const handleResize = () =>
+    setSize({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const hidden = false;
   const { darkMode, toggleDarkMode } = {
